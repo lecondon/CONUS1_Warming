c4ca7266c5973ea203349a4ff733cfcf86be9d22
lecondon <lecondon@email.arizona.edu>
2019-01-06 15:57:44 -0700

    adding to local download options

--- Data_Transfer/Cyverse_Local.sh
+++ Data_Transfer/Cyverse_Local.sh
@@ -4,6 +4,9 @@
 #Path for local directory to put files
 dir="/Users/laura/Documents/CONUS/Warming_Runs/Simulation_Averages"
 
+#PATH to Cyverse simulations
+cyvdir="/iplant/home/shared/avra/CONUS_1.0/Simulations"
+
 ##Select the runs to copy
 declare -a cyvrunlist=("Baseline" "2Deg_Warming" "4Deg_Warming") #Directory names on Cyverse
 declare -a locrunlist=("Temp0" "Temp2" "Temp4") #local directory names These will be created if they don't exist
@@ -12,7 +15,13 @@ declare -a locrunlist=("Temp0" "Temp2" "Temp4") #local directory names These wil
 declare -a varlist=("LH" "SM")
 
 # List of run years to get
-declare -a yearlist=("4" "5" "6")
+#declare -a yearlist=("4" "5" "6" "7")
+declare -a yearlist=("4" "5")
+
+#Pick whether to get daily, monthly or annual
+getday=true
+getmon=true
+getyear=true
 
 ### End of User Inputs ---------------------------
 
@@ -22,57 +31,75 @@ nvar=${#varlist[@]}
 nyear=${#yearlist[@]}
 echo "Looping over" $nrun "runs," $nvar "variables and" $nyear "years"
 
-# use for loop to read all values and indexes
+#Looping over runs
 for (( r=1; r<${nrun}+1; r++ ));
 do
-	for(( y=1; y<${nrun}+1; y++ ))
+	lrun=${locrunlist[$r-1]}
+	cyvrun=${cyvrunlist[$r-1]}
+
+	#Looping over years
+	for(( y=1; y<${nyear}+1; y++ ));
 	do
-		run=${locrunlist[r-1]}
 		year=${yearlist[$y-1]}
-		dtemp="./${run}_Year${year}"
+		echo $year
+
+		#Make a local directory for the run/year if it doesn't exist
+		dtemp="./${lrun}_Year${year}"
+		echo $dtemp
 		mkdir $dtemp
-		cd $dtemp
-		pwd
-		cd ../
-		echo $dtemp #$run $year
-  	#echo $r " / " ${nrun} " : " ${cyvrunlist[$r-1]} " : " ${locrunlist[$r-1]}
-	done
-done
+		#cd $dtemp
 
+		#Looping over variables
+		for (( v=1; v<${nvar}+1; v++ ));
+		do
+			var=${varlist[$v-1]}
+			echo $var
 
+			### Daily
+			if ($getday)
+			then
+				echo "Processing Daily"
+				#Looping over months
+				for day in {1..365}
+				do
+					#echo $day
+					fin=$(printf "%s/%s/Processed_Outputs/Year%s/%s.monthly.%03d.bin" $cyvdir $cyvrun $year $var $day)
+					fout=$(printf "%s/%s.daily.%03d.bin" $dtemp $var $day)
+					echo "Copying $fin"
+					echo "Here $fout"
+					iget -KrfP $fin $fout
+				done #end for day
+			fi #end if getday
 
+			### Monhtly
+			if ($getmon)
+			then
+				echo "Processing Monthly"
+				#Looping over months
+				for month in {1..12}
+				do
+					#echo $month
+	  			fin=$(printf "%s/%s/Processed_Outputs/Year%s/%s.monthly.%02d.bin" $cyvdir $cyvrun $year $var $month)
+					fout=$(printf "%s/%s.monthly.%02d.bin" $dtemp $var $month)
+					echo "Copying $fin"
+					echo "Here $fout"
+					iget -KrfP $fin $fout
+				done #end for month
+			fi #end if getmon
 
-#for run in "$runlist"
-#do
-#	echo "$run"
-#done
+			### Yearly
+			if ($getyear)
+			then
+				echo "Processing Yearly"
+				fin=$(printf "%s/%s/Processed_Outputs/Year%s/%s.yearly.bin" $cyvdir $cyvrun $year $var)
+				fout=$(printf "%s/%s.yearly.bin" $dtemp $var )
+				echo "Copying $fin"
+				echo "Here $fout"
+				iget -KrfP $fin $fout
+			fi #end if getmon
 
+		done #end for var
 
-#
-##pwd
-#idir="/iplant/home/shared/avra/CONUS_1.0/Simulations"
-#fname='SM.yearly.bin'
-#
-#for year in {4..7}
-#do
-#  #baseline
-#	fin="$idir/Baseline/Processed_Outputs/Year$year/$fname"
-#  fout="./Temp0_Year$year/$fname"
-#  echo $fin
-#  echo $fout
-#	#iget -KvrfP $fin $fout
-#
-#  #temp2
-#  fin="$idir/2Deg_Warming/Processed_Outputs/Year$year/$fname"
-#  fout="./Temp2_Year$year/$fname"
-#  echo $fin
-#  echo $fout
-#  #iget -KvrfP $fin $fout
-#
-#  #temp4
-#  fin="$idir/4Deg_Warming/Processed_Outputs/Year$year/$fname"
-#  fout="./Temp4_Year$year/$fname"
-#  echo $fin
-#  echo $fout
-#  #iget -KvrfP $fin $fout
-#done
+	done #end for year
+
+done #end for run
